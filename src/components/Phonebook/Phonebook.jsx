885cebb1e19f7ee990a8bae39f2b6e5887e177fc
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-07-01 15:14:45 +0300

    Done!)

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -10,27 +10,23 @@ import styles from './Phonebook.module.css';
 
 class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  // componentDidMount() {
-  //   const id = Number(localStorage.getItem('contacts-index'));
-  //   console.log(id);
-  //   if (id !== 0) {
-  //     this.setState({ id });
-  //   }
-  // }
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contact-items'));
+
+    if (contacts?.length) {
+      this.setState({
+        contacts,
+      });
+    }
+  }
 
   componentDidUpdate() {
     const { contacts } = this.state;
-    console.log(contacts);
-    localStorage.setItem('contact-item', contacts);
+    localStorage.setItem('contact-items', JSON.stringify(contacts));
   }
 
   addContact = ({ name, number }) => {
